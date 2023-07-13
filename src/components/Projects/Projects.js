574aa1c25ab2e4cd499b936b7b9d291753bbe109
Nanda Kumar Reddy Gollapalli <115801299+nandakumarreddy04@users.noreply.github.com>
2023-07-13 22:17:44 +0530

    Update Projects.js

--- src/components/Projects/Projects.js
+++ src/components/Projects/Projects.js
@@ -33,6 +33,21 @@ function Projects() {
             />
           </Col>
 
+           <Col md={4} className="project-card">
+            <ProjectCard
+              imgPath={bookhub}
+              isBlog={false}
+              title="BookHub"
+              description="BookHub-
+A react based website using openlibrary.org api to fetch a collection of books according to our search.    When it comes to searching for books, there is only one place to turn to – Book Hub.We have a veritable 
+collection of books that span categories as diverse as you could possibly expect. From segments like 
+accountancy, to parenting and pregnancy, to banking and finance, to even topics on yoga and meditation 
+almost any category you seek you will find with us."
+              ghLink="https://github.com/nandakumarreddy04/BookHub"
+              demoLink="https://bookhub-pearl.vercel.app/"              
+            />
+          </Col>
+
           <Col md={4} className="project-card">
             <ProjectCard
               imgPath={yelpcamp}
